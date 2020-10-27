cf89ce6145582eda44ec544f6d14114fe1454e53
Олександр Дибач <alex.dybach.2014@i.ua>
2020-10-27 12:03:12 +0200

    Done lesson 1 task 3

---
+++ Less001_Task3/Less001_Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Less001_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal money = 137.45m;
+            Console.WriteLine(money);
+        }
+    }
+}
